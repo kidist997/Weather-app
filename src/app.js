a6cb37128812152c7168a058a6ed8b33421fcb96
kidist997 <kidistamanuel22@gmail.com>
2024-07-20 07:48:39 +1100

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-function handleSearchSubmit(event) {
+function handleSearchSubmitt(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
 }
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmitt);
