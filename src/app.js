ee42d0d0a606ceef16ac87285f8e518159dc41bc
kidist997 <kidistamanuel22@gmail.com>
2024-07-21 08:42:06 +1100

    added API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
+function searchCity(city) {
+  let apiKey = "a03b08ef2bbd7f607a82ffef2t4o4bc4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+function refreshWeather(response) {
+  let tempratureElement = document.querySelector("#temprature");
+  let temprature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  tempratureElement.innerHTML = Math.round(temprature);
+}
+
 function handleSearchSubmitt(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmitt);
